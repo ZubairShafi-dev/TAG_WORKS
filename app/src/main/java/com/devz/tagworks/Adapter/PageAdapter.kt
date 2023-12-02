ec5a2bb6a3d9f7b93334f914dd16cc62b5d8b1f3
ranaMuzammil89 <muzammilahmad893@gmail.com>
2023-12-02 13:56:53 +0500

    bit design modified

---
+++ app/src/main/java/com/devz/tagworks/Adapter/PageAdapter.kt
@@ -0,0 +1,40 @@
+package com.devz.tagworks.Adapter
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.devz.tagworks.fragments.AdminFragment
+import com.devz.tagworks.fragments.FragmentUser
+
+class PageAdapter(fm:FragmentManager) : FragmentPagerAdapter(fm) {
+    override fun getCount(): Int {
+        return 2;
+    }
+
+    override fun getItem(position: Int): Fragment {
+         when(position) {
+            0 -> {
+                return FragmentUser()
+            }
+
+            else -> {
+                return AdminFragment()
+            }
+
+        }
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        when(position) {
+            0 -> {
+                return "Customers"
+            }
+            1 -> {
+                return "Admin"
+            }
+
+        }
+        return super.getPageTitle(position)
+    }
+
+}
--- app/src/main/java/com/devz/tagworks/fragments/AdminFragment.kt
+++ app/src/main/java/com/devz/tagworks/fragments/AdminFragment.kt
@@ -1,4 +1,4 @@
-package com.devz.tagworks.Ui
+package com.devz.tagworks.fragments
 
 import android.content.Context
 import android.content.Intent
